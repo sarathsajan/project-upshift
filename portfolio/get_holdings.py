e378df29f11aaea70dcc094d4659193422497644
sarath sajan <sarathsajan1998@gmail.com>
2026-08-09 06:02:03 +0530

    refactor(get_holdings): simplify output by printing holdings directly

--- portfolio/get_holdings.py
+++ portfolio/get_holdings.py
@@ -1,7 +1,6 @@
 # get the long term holdings of the user
 # https://upstox.com/developer/api-documentation/get-holdings
 
-from typing import cast
 import read_creds
 import upstox_client
 from upstox_client.rest import ApiException
@@ -14,16 +13,11 @@ api_instance = upstox_client.PortfolioApi(upstox_client.ApiClient(configuration)
 
 try:
     api_response = api_instance.get_holdings(api_version)
-    all_holdings = api_response.data # list of objects of type upstox_client.models.holdings_data.HoldingsData
+    all_holdings = api_response.data    # list of objects of type upstox_client.models.holdings_data.HoldingsData # type: ignore
     
     for holding in all_holdings:
-        print("company_name\t\t:", holding.company_name)
-        print("instrument_token\t:", holding.instrument_token)
-        print("average_price\t\t:", holding.average_price)
-        print("close_price\t\t:", holding.close_price)
-        print("quantity\t\t:",holding.quantity)
+        print(holding)
         print()
-
 
 except ApiException as e:
     print(f"Exception when calling ChargeApi->get_brokerage: {e}\n" )
