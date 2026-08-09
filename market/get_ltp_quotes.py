21f1737207854cee761991d1886c93fc8ea2be8e
sarath sajan <sarathsajan1998@gmail.com>
2026-08-09 06:01:51 +0530

    fix(get_ltp_quotes): update instrument key and improve terminal output handling

--- market/get_ltp_quotes.py
+++ market/get_ltp_quotes.py
@@ -2,16 +2,24 @@
 # https://upstox.com/developer/api-documentation/ltp-v3
 # https://github.com/upstox/upstox-python/blob/master/examples/market-quote/code/ltp-quotes-v3.md
 
+import os
+import subprocess
 import read_creds
 import upstox_client
 from upstox_client.rest import ApiException
 
-INSTRUMENT_KEY  = "NSE_EQ|INE848E01016"
+INSTRUMENT_KEY  = "NSE_EQ|INE669E01016"
 
 configuration = upstox_client.Configuration()
 configuration.access_token = read_creds.UPSTOX_ACCESS_TOKEN
 
 api_instance = upstox_client.MarketQuoteV3Api(upstox_client.ApiClient(configuration))
+
+
+if os.name == 'nt':
+    subprocess.run('cls', shell=True, check=False)      # Windows
+else:
+    subprocess.run('clear', shell=True, check=False)    # POSIX (Linux, macOS)
 
 try:
     api_response = api_instance.get_ltp(instrument_key=INSTRUMENT_KEY)
